8b999046986870052d44b6731ebdc532c519a803
Weiming Zhai <wmzhai@qq.com>
2016-11-07 12:15:30 +0800

    tutorial04_colored_cube

--- playground.cpp
+++ playground.cpp
@@ -61,7 +61,7 @@ int main( void )
 	glBindVertexArray(VertexArrayID);
 
 	//这里是加载当前exe目录下的shader文件的形式读取文件的
-	GLuint programID = LoadShaders("SimpleTransform.vertexshader", "SingleColor.fragmentshader");
+	GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader");
 	
 	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
@@ -79,25 +79,101 @@ int main( void )
 
 	glm::mat4 MVP = Projection * View * Model;
 
+	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
 	static const GLfloat g_vertex_buffer_data[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, -1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, -1.0f,
+		-1.0f, 1.0f, -1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, -1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, -1.0f, 1.0f
+	};
+
+	// One color for each vertex. They were generated randomly.
+	static const GLfloat g_color_buffer_data[] = {
+		0.583f, 0.771f, 0.014f,
+		0.609f, 0.115f, 0.436f,
+		0.327f, 0.483f, 0.844f,
+		0.822f, 0.569f, 0.201f,
+		0.435f, 0.602f, 0.223f,
+		0.310f, 0.747f, 0.185f,
+		0.597f, 0.770f, 0.761f,
+		0.559f, 0.436f, 0.730f,
+		0.359f, 0.583f, 0.152f,
+		0.483f, 0.596f, 0.789f,
+		0.559f, 0.861f, 0.639f,
+		0.195f, 0.548f, 0.859f,
+		0.014f, 0.184f, 0.576f,
+		0.771f, 0.328f, 0.970f,
+		0.406f, 0.615f, 0.116f,
+		0.676f, 0.977f, 0.133f,
+		0.971f, 0.572f, 0.833f,
+		0.140f, 0.616f, 0.489f,
+		0.997f, 0.513f, 0.064f,
+		0.945f, 0.719f, 0.592f,
+		0.543f, 0.021f, 0.978f,
+		0.279f, 0.317f, 0.505f,
+		0.167f, 0.620f, 0.077f,
+		0.347f, 0.857f, 0.137f,
+		0.055f, 0.953f, 0.042f,
+		0.714f, 0.505f, 0.345f,
+		0.783f, 0.290f, 0.734f,
+		0.722f, 0.645f, 0.174f,
+		0.302f, 0.455f, 0.848f,
+		0.225f, 0.587f, 0.040f,
+		0.517f, 0.713f, 0.338f,
+		0.053f, 0.959f, 0.120f,
+		0.393f, 0.621f, 0.362f,
+		0.673f, 0.211f, 0.457f,
+		0.820f, 0.883f, 0.371f,
+		0.982f, 0.099f, 0.879f
 	};
 
 	//标识vertex buffer
 	GLuint vertexbuffer;
-
-	//生成1个buffer，并将结果的标识保存在vertexbuffer里
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
-	// 把定点给OpenGL
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
 
 	do{
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(programID);
 
@@ -106,7 +182,7 @@ int main( void )
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 
-		// Draw nothing, see you in tutorial 2 !
+		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
@@ -118,9 +194,23 @@ int main( void )
 			(void*)0
 		);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// 2nd attribute buffer : colors
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+			1,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+			);
+
+
+		glDrawArrays(GL_TRIANGLES, 0, 12*3);
 
 		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -129,6 +219,13 @@ int main( void )
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colorbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
